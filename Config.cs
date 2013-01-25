befdc10785613052ce4b780ea88c0c944d6c6984
scaladev <scaladev@yandex.ua>
2013-01-25 05:29:19 -0800

    Create Config.cs

---
+++ Config.cs
@@ -0,0 +1,10 @@
+namespace ntrinity
+{
+    public sealed class Config
+    {
+        public static int APS_NEXT_RESOURCE_VALUE = 101;
+        public static int APS_NEXT_COMMAND_VALUE = 40001;
+        public static int APS_NEXT_CONTROL_VALUE = 1000;
+        public static int APS_NEXT_SYMED_VALUE = 101;
+    }
+}
